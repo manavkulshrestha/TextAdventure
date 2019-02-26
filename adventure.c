d7be45fe47cb970d67fcd32bb817bb96d21505c8
Manav Kulshrestha <manavkoolz@gmail.com>
2019-02-26 00:35:47 -0500

    Implemented add and drop commands. Untested.

--- adventure.c
+++ adventure.c
@@ -9,9 +9,16 @@
 int main() {
     char *command = (char *) malloc(10*sizeof(char));
     char *param = (char *) malloc(10*sizeof(char));
-    Item *backpack = (Item *) malloc(DEFAULT_BACKPACK_SIZE*sizeof(Item));
-    Room *current_room = (Room *) malloc(sizeOf(Room));
-    int* field[] = 
+
+    int *backpack_size = (int *) malloc(sizeof(int));
+    backpack_size = DEFAULT_BACKPACK_SIZE;
+    int *item_count = (int *) malloc(sizeof(int));
+    item_count = 0;
+
+    Item *backpack = (Item *) malloc(backpack_size*sizeof(Item));
+
+    Room *current_room = (Room *) malloc(sizeof(Room));
+    // int* field[] = 
     current_room = gen_room(3, field);
 
     do {
@@ -38,17 +45,38 @@ int main() {
             
         } else if(strcmp(command, "look") == 0) {
             
-        } else if(strcmp(command, "take") == 0) {
-            if(strcmp(command, "all") == 0) {
+        } else if(strcmp(command, "take") == 0) {//revise how freeing works
+            Item *temp = (Item *) malloc(sizeof(Item));
+            temp = item_take(param, current_room->items);
 
+            if(temp != NULL) {
+                if(item_count < backpack_size) {
+                    item_add(backpack, temp);
+                    item_count++;
+                } else {
+                    printf("Backpack is full. Increase capacity or drop an item to make space.");
+                }
             } else {
-                
+                printf("Item '%s' not found", param);
             }
+            free(temp);
+            temp = NULL;
             
         } else if(strcmp(command, "drop") == 0) {
+            Item *temp = (Item *) malloc(sizeof(Item));
+            temp = item_take(param, backpack);
+
+            if(temp != NULL) {
+                item_add(current_room->items, temp);
+                item_count--;
+            } else {
+                printf("Item '%s' not found", param);
+            }
+            free(temp);
+            temp = NULL;
 
         } else if(strcmp(command, "use") == 0) {
-
+            
         }
 
     } while(1)
